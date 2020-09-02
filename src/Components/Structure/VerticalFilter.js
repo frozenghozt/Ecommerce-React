ecbc1547d3204113830730fcba4ef230997e075c
Diogo Silva <diogo.silva.3439.tdgo14@gmail.com>
2020-09-02 20:55:06 +0100

    Netlify

--- src/Components/Structure/VerticalFilter.js
+++ src/Components/Structure/VerticalFilter.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { motion, AnimatePresence } from "framer-motion";
-import { Link } from "react-router-dom";
 
 const Container = styled.div`
   display: none;
